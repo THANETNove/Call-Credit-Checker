7d5111be7992073d17f85f97c94ca9655e449149
Thanet saiwan <thanetsaiwan@Thanets-MacBook-Air.local>
2023-06-29 15:01:49 +0700

    new changes

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableHighlight} from 'react-native';
+export default class App extends React.Component {
+  _onPressButton = () => {
+    console.log('You pressed the button');
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Should the detection be on?</Text>
+        <TouchableHighlight onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.text}>Press here</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'honeydew',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  text: {
+    padding: 20,
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: 'aquamarine',
+    width: 200,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
